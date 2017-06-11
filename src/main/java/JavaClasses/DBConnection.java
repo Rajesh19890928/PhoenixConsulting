a5133927133fb68ba40dce27ef3220294c37f3ce
raojha <raojha@USBLRRAOJHA9.us.deloitte.com>
2017-06-11 22:45:28 +0530

    Initial commit

--- src/main/java/JavaClasses/DBConnection.java
+++ src/main/java/JavaClasses/DBConnection.java
@@ -9,11 +9,15 @@ package JavaClasses;
  *
  * @author raojha
  */
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DBConnection {
     public ResultSet getSubjects()
@@ -28,7 +32,9 @@ public class DBConnection {
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://20.0.1.231:3306/mynewdatabase","root","tiger");
+            String ipaddress = read_DB_IP();
+            String url = "jdbc:mysql://"+ipaddress+":3306/mynewdatabase";
+            Connection con = DriverManager.getConnection(url,"root","tiger");
             String query = "SELECT DISTINCT subject_id, name FROM subject";
             PreparedStatement statement = con.prepareStatement(query);
             rs = statement.executeQuery();
@@ -54,7 +60,9 @@ public class DBConnection {
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://20.0.1.231:3306/mynewdatabase","root","tiger");
+            String ipaddress = read_DB_IP();
+            String url = "jdbc:mysql://"+ipaddress+":3306/mynewdatabase";
+            Connection con = DriverManager.getConnection(url,"root","tiger");
             String query = "SELECT * FROM counselor AS c INNER JOIN subject AS s ON c.counselor_id=s.counselor_idfk WHERE s.subject_id="+subject_id;
             PreparedStatement statement = con.prepareStatement(query);
             rs = statement.executeQuery();
@@ -71,5 +79,18 @@ public class DBConnection {
             System.out.println("VendorError: " + ex.getErrorCode());
         }
         return rs;
+    }
+    
+    public String read_DB_IP()
+    {
+        java.io.InputStream is = getClass().getClassLoader().getResourceAsStream("my.properties");
+        java.util.Properties p = new Properties();
+        try {
+            p.load(is);
+        } catch (IOException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String name = p.getProperty("databse_url");
+        return name;
     }
 }
